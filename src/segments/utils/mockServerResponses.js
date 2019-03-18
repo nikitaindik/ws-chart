55e76c154b3d3c87d6a92d5231005d5c29335b18
Nikita Indik <nikita.indik@exness.com>
2019-03-18 08:26:55 +0200

    Fix segment size going below zero

--- src/segments/utils/mockServerResponses.js
+++ src/segments/utils/mockServerResponses.js
@@ -1,30 +1,49 @@
 import { socketEventTypes } from '../constants';
 import generateFakeDataForDisplay from '../utils/fakeDataGenerator';
 
+const getSegmentSize = data => {
+  const dataSample = Object.values(data)[0];
+  return dataSample[dataSample.length - 1].segmentSize;
+};
+
 const handleSegmentHistoryRequest = (socket, message) => {
+  const fakeData = generateFakeDataForDisplay();
+
+  // Keep track of the current segment size so we don't drop it below zero
+  let segmentSize = getSegmentSize(fakeData);
+
   const segmentId = message.payload;
   socket.send(
     JSON.stringify({
       type: socketEventTypes.SEGMENT_HISTORY,
       payload: {
         segmentId,
-        data: generateFakeDataForDisplay(),
+        data: fakeData,
       },
     }),
   );
 
   setInterval(() => {
+    let change = Math.random() > 0.5 ? 1 : -1;
+
+    const willDropBelowZero = segmentSize + change < 0;
+    if (willDropBelowZero) {
+      change = 1;
+    }
+
+    segmentSize += change;
+
     socket.send(
       JSON.stringify({
         type: socketEventTypes.SEGMENT_UPDATE,
         data: {
           segmentId,
           timestamp: Date.now(),
-          change: Math.random() > 0.5 ? 1 : -1,
+          change,
         },
       }),
     );
-  }, 5000);
+  }, 1000);
 };
 
 const mockServerResponses = (socket, message) => {
